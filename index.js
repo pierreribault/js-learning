08f27a1e539ade2524e3656fbbdf4c2721652e51
Pierre Ribault <contact@pierreribault.fr>
2020-07-30 10:17:21 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+/**
+ * Player 1 choose a number
+ */
+const setNumber = () => {
+    return new Promise((resolve, reject) => {
+        rl.question(`Choose a number between 0-100: `, (data) => {
+            if (data >= 0 && data <= 100) {
+                resolve({value: data})
+            } else {
+                reject({message : `Invalid input`})
+            }
+        }) 
+    })
+}
+
+/**
+ * Player 2 try to find the number
+ * @param {*} value 
+ */
+const findNumber = (value) => {
+    return new Promise((resolve, reject) => {
+        rl.question(`\nProposition: `, (anwser) => {
+            if (value == anwser) {
+                resolve({message: `Nice job, it's the good awnser`})
+            } else if (isNaN(anwser)) {
+                reject({message: `Invalid input`})
+            } else if (parseInt(anwser) > parseInt(value)) {
+                reject({message: `It's less than ${anwser}`})
+            } else {
+                reject({message: `It's more than ${anwser}`})
+            }
+        })
+    })
+}
+
+/**
+ * Player 2 step
+ * @param {*} value 
+ */
+const step = async (value) => {
+    await findNumber(value).then(
+        success => {
+            console.log(success.message)
+            rl.close()
+        },
+        error => {
+            console.log(error.message)
+            step(value)
+        }
+    )
+}
+
+/**
+ *  Start of the game
+ */
+const main = async () => {
+    console.log('Player 1:')
+    await setNumber().then(
+        data => {
+            console.clear()
+            console.log('Player 2:')
+            step(data.value)
+        },
+        error => {
+            main()
+        }
+    )
+}
+
+main()
